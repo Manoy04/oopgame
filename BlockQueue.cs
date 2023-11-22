1ef0b587b254225067df7ec2cc39cec4db3e1785
Dylpuf <131676226+dylpuf@users.noreply.github.com>
2023-11-22 15:26:08 +0100

    Add files via upload

---
+++ BlockQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Documents;
+using System.Windows.Media.Animation;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public Block Queue()
+        {
+        NextBlock = RandomBlock();
+        }
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetandUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = randomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
